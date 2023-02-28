a458ca084605fbeda919b887b66146d8d0525236
Ady Toledano <adyto23@gmail.com>
2023-02-28 16:56:31 +0100

    Added new files

---
+++ firstsqltask.sql
@@ -0,0 +1,21 @@
+USE sakila;
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+SELECT title FROM film;
+
+SELECT * FROM language;
+SELECT name FROM language;
+
+SELECT * FROM sales_by_store;
+
+SELECT * FROM store;
+
+SELECT COUNT(store_id) FROM store;
+
+SELECT * from staff;
+
+SELECT first_name from staff
+
+
+
